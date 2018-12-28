91f37407dc40bf5e12f159c52c810ab57446b146
Joey <joey@lineageos.org>
2018-12-28 11:33:30 +0100

    msm8998-common: dirac: Introduce Dirac preferences
    Add a frontend for Dirac sound optimization settings.
    Inject into sound preferences through settings IA.
    
    Change-Id: I37f6e64fa440c43dbe238cc65af5f9b0bbd60d83
    Signed-off-by: Joey <joey@lineageos.org>
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    Signed-off-by: Henrique Pereira <pereirah2001@gmail.com>

--- common.mk
+++ common.mk
@@ -157,6 +157,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    Dirac \
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
